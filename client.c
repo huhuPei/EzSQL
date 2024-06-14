16a0a2b4c46aec3f4002fb9d83c0e4552758f65b
huhuPei <291921096@qq.com>
2024-06-14 16:32:50 +0800

    添加sql解析器与执行器

--- client.c
+++ client.c
@@ -10,10 +10,33 @@ typedef struct {
     /* data */
 } InputBuffer;
 
+typedef enum {
+    META_COMMAND_SUCCESS,
+    META_COMMAND_EXIT,
+    META_COMMAND_INVALID_COMMAND
+} MetaCommandResult;
+
+typedef enum { 
+    PREPARE_SUCCESS, 
+    PREPARE_UNRECOGNIZED_STATEMENT 
+} PrepareResult;
+
+typedef enum { 
+    STATEMENT_INSERT, 
+    STATEMENT_SELECT 
+} StatementType;
+
+typedef struct {
+  StatementType type;
+} Statement;
+
 InputBuffer* new_input_buffer();
 void close_input_buffer(InputBuffer*);
 void read(InputBuffer*);
 void print_prompt();
+MetaCommandResult do_meta_command(const char*);
+PrepareResult prepare_statement(const char*, Statement*);
+void execute_statement(Statement*);
 
 InputBuffer* new_input_buffer() {
     InputBuffer* input_buffer = (InputBuffer*) malloc(sizeof(InputBuffer));
@@ -22,7 +45,6 @@ InputBuffer* new_input_buffer() {
     input_buffer->buffer = NULL;    
     return input_buffer;
 }
-
 
 void close_input_buffer(InputBuffer* input_buffer) {
     free(input_buffer->buffer);
@@ -38,19 +60,71 @@ void read(InputBuffer* input_buffer) {
     input_buffer->input_size = bytes_read - 1;
     input_buffer->buffer[bytes_read - 1] = '\0';
 }
+
 void print_prompt() { printf("db > "); }
+
+MetaCommandResult do_meta_command(const char* command) {
+    if (strcmp(command, ":exit") == 0) {
+        return META_COMMAND_EXIT;
+    } else {
+        return META_COMMAND_INVALID_COMMAND;
+    }
+}
+
+PrepareResult prepare_statement(const char* source, Statement* statement) {
+    if (strncmp(source, "insert", 6) == 0) {
+        statement->type = STATEMENT_INSERT;
+        return PREPARE_SUCCESS;
+    }
+    if (strcmp(source, "select") == 0) {
+        statement->type = STATEMENT_SELECT; 
+        return PREPARE_SUCCESS;
+    }
+    return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+void execute_statement(Statement* statement) {
+    switch (statement->type) {
+        case STATEMENT_INSERT:
+            printf("This is where we would do an insert.\n");
+            break;
+        case STATEMENT_SELECT:
+            printf("This is where we would do a select.\n");
+            break;
+    }
+}
 
 int main(int argc, char* argv[]) {
     InputBuffer* input_buffer = new_input_buffer();
-    while (true) {
+    bool running = true;
+    while (running) {
         print_prompt();
-        read(input_buffer);         
-        if (strcmp(input_buffer->buffer, ":exit") == 0) {
-            close_input_buffer(input_buffer);
-            printf("Bye.\n");
-            exit(EXIT_SUCCESS);
+        read(input_buffer);
+        if (input_buffer->buffer[0] == ':') {
+            switch (do_meta_command(input_buffer->buffer)){
+                case META_COMMAND_EXIT:
+                    close_input_buffer(input_buffer);
+                    printf("Bye.\n");
+                    exit(EXIT_SUCCESS);
+                case META_COMMAND_SUCCESS:
+                    break;
+                case META_COMMAND_INVALID_COMMAND:
+                    printf("Invalid command '%s'.\n", input_buffer->buffer);
+                    break;  
+                default: ;;
+            }
         } else {
-            printf("Invalid command '%s'.\n", input_buffer->buffer);
-        }  
+            Statement statement;
+            switch (prepare_statement(input_buffer->buffer, &statement)) {
+                case PREPARE_SUCCESS:
+                    break;
+                case PREPARE_UNRECOGNIZED_STATEMENT:
+                    printf("Unrecognized keyword at start of '%s'.\n", input_buffer->buffer);
+                    continue;
+                default: continue;
+            }
+            execute_statement(&statement);
+            printf("Executed.\n");
+        }
     }
 }
